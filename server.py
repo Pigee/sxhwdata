1d79696dcee8c10444ced539d216d4e86d83cc97
pigee <lijp@localhost.localdomain>
2023-05-13 20:09:02 +0800

    add procedure

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from sanic.response import text
 from sanic.response import json
 import pymysql
 
-db = pymysql.connect( # 创建数据库连接
+csdb = pymysql.connect( # 创建数据库连接
     host='192.168.1.11', # 要连接的数据库所在主机ip
     port=3306, # 数据库端口
     database='cs_y_run_nxsw',
@@ -13,14 +13,15 @@ db = pymysql.connect( # 创建数据库连接
     #cursorclass=cursors.DictCursor
 )
 
-cur = db.cursor(pymysql.cursors.DictCursor)
+cur = csdb.cursor(pymysql.cursors.DictCursor)
 
-app = Sanic("MyHelloWorldApp")
+app = Sanic("sxhwdata")
 
 @app.get("/")
 async def hello_world(request):
     return text("Hello, world.")
 
+# get 请求
 @app.get("/j")
 async def get_json(request):
     sql = "select id,concat(create_date,'')create_date,time_accept,repair,time_accept,time_receiver,time_handle,remark,gd_result,appraise,dz_address from w_1592374411638 where create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and time_handle is null"
@@ -28,6 +29,7 @@ async def get_json(request):
     all_obj = cur.fetchall()
     return json(all_obj,ensure_ascii=False)
 
+# post 请求
 @app.post("/p")
 async def post_json(request):
     print(request.json["start_date"])
@@ -35,6 +37,16 @@ async def post_json(request):
     cur.execute(sql,(request.json["start_date"],request.json["end_date"]))
     all_obj = cur.fetchall()
     return json(all_obj,ensure_ascii=False)
+
+# 调用存储过程
+@app.post("/proc")
+async def proc_json(request):
+    print(request.json["name"])
+    sql = "call proc_t(%s)"
+    cur.execute(sql,(request.json["name"]))
+    all_obj = cur.fetchall()
+    return json(all_obj,ensure_ascii=False)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1337, dev=True)
