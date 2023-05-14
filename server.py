3251ce97a1048291429539a2f3aae37ac7927b74
pigee <lijp@localhost.localdomain>
2023-05-14 21:36:28 +0800

    step forward

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
 from sanic import Sanic
+from sanic import response
 from sanic.response import text
-from sanic.response import json
+from sanic.response import json,json_dumps
 import pymysql
+import json as rjson
 
-csdb = pymysql.connect( # 创建数据库连接
+db_liupanshan = pymysql.connect( # 创建数据库连接
     host='192.168.1.11', # 要连接的数据库所在主机ip
     port=3306, # 数据库端口
     database='cs_y_run_nxsw',
@@ -13,20 +15,48 @@ csdb = pymysql.connect( # 创建数据库连接
     #cursorclass=cursors.DictCursor
 )
 
-cur = csdb.cursor(pymysql.cursors.DictCursor)
+db_quarkcalldb = pymysql.connect( # 创建数据库连接
+    host='192.168.1.11', # 要连接的数据库所在主机ip
+    port=3306, # 数据库端口
+    database='quarkcalldb',
+    user='sxadmin', # 数据库登录用户名
+    password='sx@123', # 登录用户密码
+    charset='utf8mb4' # 编码，注意不能写成utf-8
+    #cursorclass=cursors.DictCursor
+)
+
+
+cur_liupanshan = db_liupanshan.cursor(pymysql.cursors.DictCursor)
+#cur_quarkcalldb = db_quarkcalldb.cursor()
+cur_quarkcalldb = db_quarkcalldb.cursor(pymysql.cursors.DictCursor)
 
 app = Sanic("sxhwdata")
 
 @app.get("/")
 async def hello_world(request):
-    return text("Hello, world.")
+    return text("Hello, sx world.")
+
+# 调用存储过程
+@app.post("/proc")
+async def proc_json(request):
+    print(request.json["name"])
+    sql = "call proc_t(%s)"
+    cur_liupanshan.execute(sql,(request.json["name"]))
+    all_obj = cur_liupanshan.fetchone()
+
+    sql = "select GroupNo WorkerNo,count(1) OutsideCallTotalCnt from tbcallcdr where calltime >= %s and calltime <= %s and length(CallerNo) > 4 and GroupNo = %s"
+    cur_quarkcalldb.execute(sql,(request.json["start_date"],request.json["end_date"],request.json["group_no"]))
+    fd_obj = cur_quarkcalldb.fetchone()
+    all_obj["OutsideCallTotalCnt"] = fd_obj["OutsideCallTotalCnt"]
+    return json(all_obj,ensure_ascii=False)
+
 
 # get 请求
 @app.get("/j")
 async def get_json(request):
     sql = "select id,concat(create_date,'')create_date,time_accept,repair,time_accept,time_receiver,time_handle,remark,gd_result,appraise,dz_address from w_1592374411638 where create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and time_handle is null"
-    cur.execute(sql)
-    all_obj = cur.fetchall()
+    cur_liupanshan.execute(sql)
+    all_obj = cur_liupanshan.fetchall()
     return json(all_obj,ensure_ascii=False)
 
 # post 请求
@@ -34,19 +64,9 @@ async def get_json(request):
 async def post_json(request):
     print(request.json["start_date"])
     sql = "select id,concat(create_date,'')create_date,time_accept,repair,time_accept,time_receiver,time_handle,remark,gd_result,appraise,dz_address from w_1592374411638 where create_date >= %s and create_date <= %s and time_handle is null"
-    cur.execute(sql,(request.json["start_date"],request.json["end_date"]))
-    all_obj = cur.fetchall()
+    cur_liupanshan.execute(sql,(request.json["start_date"],request.json["end_date"]))
+    all_obj = cur_liupanshan.fetchall()
     return json(all_obj,ensure_ascii=False)
-
-# 调用存储过程
-@app.post("/proc")
-async def proc_json(request):
-    print(request.json["name"])
-    sql = "call proc_t(%s)"
-    cur.execute(sql,(request.json["name"]))
-    all_obj = cur.fetchall()
-    return json(all_obj,ensure_ascii=False)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1337, dev=True)
---
+++ test.sql
@@ -0,0 +1,59 @@
+-- 派单处理
+select count(t.id) sl from (select id from w_1592374411638 where 
+create_date >= '2023-04-25 00:00:00' and create_date <= '2023-04-25 23:59:59' 
+union
+select id from  nxsw_1609840221906 where 
+create_date >= '2023-04-25 00:00:00' and create_date <= '2023-04-25 23:59:59') t;
+
+-- 接单超时
+select count(t.id) sl from (select id,dz_address,`repair`,position,create_date,time_accept,time_receiver,time_handle,remark,gd_result,appraise from w_1592374411638 where 
+create_date >= '2023-04-25 00:00:00' and create_date <= '2023-04-25 23:59:59'  and timestampdiff(minute,time_accept,time_receiver) >= 20
+  union
+select id,dz_address,'' ,'' ,create_date,time_accept,time_receiver,time_handle,remark,gd_result,appraise from nxsw_1609840221906 where 
+create_date >= '2023-04-25 00:00:00' and create_date <= '2023-04-25 23:59:59' and timestampdiff(minute,time_accept,time_receiver)>= 20) t;
+-- 维修超时
+select count(t.id) sl from (select id,dz_address,repair,position,create_date,time_accept,time_receiver,time_handle,remark,gd_result,appraise from w_1592374411638 where 
+create_date >= '2023-04-25 00:00:00' and create_date <= '2023-04-25 23:59:59' and repair = '小修'  and timestampdiff(hour,time_receiver,time_handle) > 8
+union 
+select id,dz_address,repair,position,create_date,time_accept,time_receiver,time_handle,remark,gd_result,appraise from w_1592374411638 where 
+create_date >= '2023-04-25 00:00:00' and create_date <= '2023-04-25 23:59:59' and repair = '大修'  and timestampdiff(hour,time_receiver,time_handle) > 24) t;
+
+-- 不满意事件
+select id,dz_address,repair,position,create_date,time_accept,time_receiver,time_handle,remark,gd_result,appraise from w_1592374411638 where 
+create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and appraise ='不满意';
+
+-- 接单后未处理回复事件
+select id,dz_address,repair,position,create_date,time_accept,time_receiver,time_handle,remark,gd_result,appraise from w_1592374411638 where  create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and time_handle is null;
+-- 故障报修 未完结
+select id,dz_address,repair,position,create_date,time_accept,time_receiver,time_handle,remark,gd_result,appraise from w_1592374411638 where  create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and time_handle is null;
+
+-- 故障报修 未完结
+select id,dz_address,create_date,time_accept,time_receiver,time_handle,remark,gd_result,appraise from nxsw_1609840221906 where  create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and time_handle is null;
+
+-- 共受理热线来电
+select GroupNo WorkerNo,hour(calltime) tm,count(1) OutsideCallTotalCnt from tbcallcdr where calltime >= '2023-04-22' and calltime <= '2023-04-26 23:59:59' and length(CallerNo) > 4 and GroupNo = '1';
+
+	select t1.sl 派单处理,t2.sl 接单超时,t3.sl 维修超时,t4.sl 不满意 from (
+select 1 id,count(a.id) sl from (select id from w_1592374411638 where 
+create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' 
+union
+select id from  nxsw_1609840221906 where 
+create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59') a) t1
+join
+ (
+select 1 id, count(a.id) sl from (select id from w_1592374411638 where 
+create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and ROUND((UNIX_TIMESTAMP(time_receiver)-UNIX_TIMESTAMP(time_accept))/60) > 20
+  union
+select id from nxsw_1609840221906 where 
+create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and ROUND((UNIX_TIMESTAMP(time_receiver)-UNIX_TIMESTAMP(time_accept))/60) > 20) a
+) t2 
+join
+(select count(a.id) sl from (
+select id from w_1592374411638 where create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and repair = '小修'  and round((round(UNIX_TIMESTAMP(time_handle))-round(UNIX_TIMESTAMP(time_receiver)))/3600) > 8
+union 
+select id from w_1592374411638 where create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and repair = '大修'  and  round((round(UNIX_TIMESTAMP(time_handle))-round(UNIX_TIMESTAMP(time_receiver)))/3600)>24 )a ) t3
+join
+(
+  select count(1) sl from w_1592374411638 where 
+create_date >= '2023-03-01 00:00:00' and create_date <= '2023-04-25 23:59:59' and appraise ='不满意'
+) t4 ;
