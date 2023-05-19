61483646bb052cebe3f73149038a3f0ed4dd0d4c
pigee <lijp@localhost.localdomain>
2023-05-19 13:10:12 +0800

    wuzhong new model

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,6 @@
 from sanic import Blueprint
 from .liupanshan_bp import liupanshanbp
-# from .wuzhong_bp import wuzhongbp
+from .wuzhong_bp import wuzhongbp
 
-api = Blueprint.group(liupanshanbp,url_prefix="/api")
-#api = Blueprint.group(liupanshanbp,wuzhongbp,url_prefix="/api")
+#api = Blueprint.group(liupanshanbp,url_prefix="/api")
+api = Blueprint.group(liupanshanbp,wuzhongbp,url_prefix="/api")
