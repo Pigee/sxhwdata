b15d593e2a1da588baa68521c39bf753a640fc1f
Pigee <haiou511@gmail.com>
2023-07-13 10:56:40 +0800

    delete password

--- conf/mysqldb.py
+++ conf/mysqldb.py
@@ -16,12 +16,11 @@ def Callconn():
 
 def Csconn():
     db_cs = pymysql.connect( # 创建数据库连接
-    host='36.103.224.108', # 要连接的数据库所在主机ip
-    #host='192.168.30.71', # 要连接的数据库所在主机ip
-    port=21111, # 数据库端口
+    host='192.168.199.100', # 要连接的数据库所在主机ip
+    port=3306, # 数据库端口
     #database='cs_y_run_nxsw_liupanshan',
     user='sxadmin', # 数据库登录用户名
-    password='Sxxm@123', # 登录用户密码
+    password='sx@123', # 登录用户密码
     charset='utf8mb4' # 编码，注意不能写成utf-8
     #cursorclass=cursors.DictCursor
     )
